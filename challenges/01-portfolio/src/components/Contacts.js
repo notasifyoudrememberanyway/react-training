80a232ff960a8718894cc6f6e301208a2ec097d8
surajverma2587 <surajverma2587@gmail.com>
2023-01-01 20:37:36 +0000

    portfolio challenge

---
+++ challenges/01-portfolio/src/components/Contacts.js
@@ -0,0 +1,18 @@
+export const Contacts = () => {
+  return (
+    <div className="contact-links-container">
+      <a href="#" className="contact-icon">
+        <i className="fa-brands fa-linkedin"></i>
+      </a>
+      <a href="#" className="contact-icon">
+        <i className="fa-solid fa-envelope"></i>
+      </a>
+      <a href="#" className="contact-icon">
+        <i className="fa-brands fa-twitter"></i>
+      </a>
+      <a href="#" className="contact-icon">
+        <i className="fa-solid fa-phone"></i>
+      </a>
+    </div>
+  );
+};
---
+++ challenges/01-portfolio/src/components/Projects.js
@@ -0,0 +1,130 @@
+import weatherApp from "../images/weather-app.jpg";
+
+export const Projects = () => {
+  return (
+    <div className="projects" id="projects">
+      <div className="container py-5">
+        <h1 className="text-center">My Projects</h1>
+        <h2 className="fs-5 text-center text-muted">
+          Full-stack projects using React
+        </h2>
+        <hr />
+        <div className="projects-container">
+          <div className="card project-card">
+            <img
+              src={weatherApp}
+              className="card-img-top project-card-image"
+              alt="weather forecast application"
+            />
+            <div className="card-body text-center">
+              <h3 className="card-title fs-4">Weather Dashboard</h3>
+              <div className="card-links-container">
+                <a href="#" className="card-links-icon">
+                  <i className="fa-solid fa-globe"></i>
+                </a>
+                <a href="#" className="card-links-icon">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="card project-card">
+            <img
+              src={weatherApp}
+              className="card-img-top project-card-image"
+              alt="weather forecast application"
+            />
+            <div className="card-body text-center">
+              <h3 className="card-title fs-4">Weather Dashboard</h3>
+              <div className="card-links-container">
+                <a href="#" className="card-links-icon">
+                  <i className="fa-solid fa-globe"></i>
+                </a>
+                <a href="#" className="card-links-icon">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="card project-card">
+            <img
+              src={weatherApp}
+              className="card-img-top project-card-image"
+              alt="weather forecast application"
+            />
+            <div className="card-body text-center">
+              <h3 className="card-title fs-4">Weather Dashboard</h3>
+              <div className="card-links-container">
+                <a href="#" className="card-links-icon">
+                  <i className="fa-solid fa-globe"></i>
+                </a>
+                <a href="#" className="card-links-icon">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="card project-card">
+            <img
+              src={weatherApp}
+              className="card-img-top project-card-image"
+              alt="weather forecast application"
+            />
+            <div className="card-body text-center">
+              <h3 className="card-title fs-4">Weather Dashboard</h3>
+              <div className="card-links-container">
+                <a href="#" className="card-links-icon">
+                  <i className="fa-solid fa-globe"></i>
+                </a>
+                <a href="#" className="card-links-icon">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="card project-card">
+            <img
+              src={weatherApp}
+              className="card-img-top project-card-image"
+              alt="weather forecast application"
+            />
+            <div className="card-body text-center">
+              <h3 className="card-title fs-4">Weather Dashboard</h3>
+              <div className="card-links-container">
+                <a href="#" className="card-links-icon">
+                  <i className="fa-solid fa-globe"></i>
+                </a>
+                <a href="#" className="card-links-icon">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="card project-card">
+            <img
+              src={weatherApp}
+              className="card-img-top project-card-image"
+              alt="weather forecast application"
+            />
+            <div className="card-body text-center">
+              <h3 className="card-title fs-4">Weather Dashboard</h3>
+              <div className="card-links-container">
+                <a href="#" className="card-links-icon">
+                  <i className="fa-solid fa-globe"></i>
+                </a>
+                <a href="#" className="card-links-icon">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
